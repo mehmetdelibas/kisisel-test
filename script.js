9f1d0545771125e28db67d289b141724239694f1
metin delibas <158474229+mehmetdelibas@users.noreply.github.com>
2026-01-08 20:34:02 +0300

    add script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+function hesapla() {
+  let skor = { A:0, B:0, C:0, D:0 };
+  let puan = { A:4, B:3, C:2, D:1 };
+
+  for(let i=1;i<=14;i++){
+    let secim = document.querySelector(`input[name="q${i}"]:checked`);
+    if(!secim){
+      alert("Lütfen tüm soruları cevaplayın");
+      return;
+    }
+    skor[secim.value] += puan[secim.value];
+  }
+
+  let maxSkor = Math.max(...Object.values(skor));
+  let enYuksekler = Object.keys(skor).filter(k => skor[k] === maxSkor);
+
+  // EŞİTLİK VARSA RANDOM
+  let secilen = enYuksekler[Math.floor(Math.random() * enYuksekler.length)];
+
+  let sonucMetni = {
+    A: "🦁 PANTER – Kararlı, sonuç odaklı ve lider ruhlu",
+    B: "🦚 TAVUSKUŞU – Sosyal, enerjik ve motive edici",
+    C: "🐬 YUNUS – Empatik, uyumlu ve ilişki odaklı",
+    D: "🦉 BAYKUŞ – Analitik, planlı ve mantıklı"
+  };
+
+  document.getElementById("sonuc").innerHTML = `
+    <p>Sonucunuz:</p>
+    <strong>${sonucMetni[secilen]}</strong>
+  `;
+}
